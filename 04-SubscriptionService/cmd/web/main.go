5aff23da2ba807698e229faacbac6edaa34a947c
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-08 01:01:54 +0530

    add code to connect to postgredb

--- 04-SubscriptionService/cmd/web/main.go
+++ 04-SubscriptionService/cmd/web/main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
 	// connect to database
+	db := initDB()
+	db.Ping()
 
 	// create sessions
 
@@ -17,4 +30,48 @@ func main() {
 
 	// listen to the web connections
 
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("cannot connect to database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet connected....")
+		} else {
+			log.Println("Connected to Postgres!!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for 1 sec.")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
