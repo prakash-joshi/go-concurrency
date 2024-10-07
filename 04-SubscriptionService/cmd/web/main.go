95789db56d945df23abbe71a8bf2eb55340e309c
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-07 23:41:09 +0530

    initial commit 04-subscriptionservice project

---
+++ 04-SubscriptionService/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to database
+
+	// create sessions
+
+	// create channels
+
+	// create wait group
+
+	// setup the application config
+
+	// set up mail
+
+	// listen to the web connections
+
+}
---
+++ 04-SubscriptionService/go.mod
@@ -0,0 +1,3 @@
+module subscription-service
+
+go 1.22.2
