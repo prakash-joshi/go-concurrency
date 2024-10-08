9ee473613180acdafcb640111035ce9d1ee32bb4
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-08 09:51:35 +0530

    add sessions and redis initialization functions

--- 04-SubscriptionService/cmd/web/main.go
+++ 04-SubscriptionService/cmd/web/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -19,7 +23,7 @@ func main() {
 	db.Ping()
 
 	// create sessions
-
+	session := initSession()
 	// create channels
 
 	// create wait group
@@ -74,4 +78,28 @@ func openDB(dsn string) (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	//set up session
+	session := scs.New()
+
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
 }
