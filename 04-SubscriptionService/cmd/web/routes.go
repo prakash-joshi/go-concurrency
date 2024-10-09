2e6ebf3b4024a6d598b4da8e4e12b9d6c4f2de27
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-09 23:50:50 +0530

    add handler and routes files

---
+++ 04-SubscriptionService/cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *Config) routes() http.Handler {
+
+	// create router
+	mux := chi.NewRouter()
+
+	// setup middleware
+	mux.Use(middleware.Recoverer)
+
+	// define application routes
+	mux.Get("/", app.HomePage)
+
+	return mux
+}
