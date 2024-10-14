1f6ade89eb04a9ead56a2c8cd12fb3a49ef02aa1
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-14 12:11:01 +0530

    add PostLoginPage handler code

--- 04-SubscriptionService/cmd/web/handlers.go
+++ 04-SubscriptionService/cmd/web/handlers.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "home.page.gohtml", nil)
@@ -11,7 +14,46 @@ func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+	_ = app.Sessions.RenewToken(r.Context())
 
+	// parse from post
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// get email and password from form post
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	user, err := app.Models.User.GetByEmail(email)
+	if err != nil {
+		app.Sessions.Put(r.Context(), "error", "invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	// check password
+	validPassword, err := user.PasswordMatches(password)
+
+	if err != nil {
+		app.Sessions.Put(r.Context(), "error", "invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if !validPassword {
+		app.Sessions.Put(r.Context(), "error", "invalid credentials.")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// okay to log user in
+	app.Sessions.Put(r.Context(), "userID", user.ID)
+	app.Sessions.Put(r.Context(), "user", user)
+	app.Sessions.Put(r.Context(), "flash", fmt.Sprintln("Successful logged in ", user.FirstName+" "+user.LastName))
+
+	// redirect the user
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
