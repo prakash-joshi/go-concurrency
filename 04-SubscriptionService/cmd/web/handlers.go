2095b5bf4f0dabaed38adc1521f46e00f00f53c9
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-25 23:48:24 +0530

    fix typo

--- 04-SubscriptionService/cmd/web/handlers.go
+++ 04-SubscriptionService/cmd/web/handlers.go
@@ -261,7 +261,7 @@ func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
 	importer := gofpdi.NewImporter()
 	time.Sleep(5 * time.Second)
 
-	t := importer.ImportPage(pdf, "./pdf/manual.pdf", 1, "Media")
+	t := importer.ImportPage(pdf, "./pdf/manual.pdf", 1, "/MediaBox")
 	pdf.AddPage()
 
 	importer.UseImportedTemplate(pdf, t, 0, 0, 215.9, 0)
@@ -272,7 +272,7 @@ func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
 	pdf.SetFont("Arial", "", 12)
 	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
 	pdf.Ln(5)
-	pdf.MultiCell(0, 4, fmt.Sprintf("%s user Guide"), "", "c", false)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", plan.PlanName), "", "C", false)
 
 	return pdf
 }
