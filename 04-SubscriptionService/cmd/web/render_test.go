935d46137f2c1cd4805b471cbf19bfc219647005
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-29 23:49:17 +0530

    add default test session data for flash, error and warning

---
+++ 04-SubscriptionService/cmd/web/render_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestConfig_AddDefault(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	ctx := getCtx(req)
+
+	req = req.WithContext(ctx)
+
+	testApp.Sessions.Put(ctx, "flash", "flash")
+	testApp.Sessions.Put(ctx, "warning", "warning")
+	testApp.Sessions.Put(ctx, "error", "error")
+
+	td := testApp.AddDefaultData(&TemplateData{}, req)
+
+	if td.Flash != "flash" {
+		t.Error("failed to get flash data")
+	}
+
+	if td.Warning != "warning" {
+		t.Error("failed to get warning data")
+	}
+
+	if td.Error != "error" {
+		t.Error("failed to get error data")
+	}
+
+}
