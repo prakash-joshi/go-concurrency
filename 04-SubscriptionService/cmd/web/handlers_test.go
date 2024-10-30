faa8c5e242908918ef3d1c104007e01c0a036d2c
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-10-31 00:50:38 +0530

    add test for handlers and provide the routes and handler func in table for the test function

---
+++ 04-SubscriptionService/cmd/web/handlers_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"subscription-service/data"
+	"testing"
+)
+
+var pageTests = []struct {
+	name               string
+	url                string
+	expectedStatusCode int
+	handler            http.HandlerFunc
+	sessionData        map[string]any
+	expectedHTML       string
+}{
+	{
+		name:               "home",
+		url:                "/",
+		expectedStatusCode: http.StatusOK,
+		handler:            testApp.HomePage,
+	},
+	{
+		name:               "login page",
+		url:                "/login",
+		expectedStatusCode: http.StatusOK,
+		handler:            testApp.LoginPage,
+		expectedHTML:       `<h1 class="mt-5">Login</h1>`,
+	},
+	{
+		name:               "logout page",
+		url:                "/logout",
+		expectedStatusCode: http.StatusSeeOther,
+		handler:            testApp.Logout,
+		sessionData: map[string]any{
+			"userID": 1,
+			"user":   data.User{},
+		},
+	},
+}
+
+func TestConfig_Pages(t *testing.T) {
+	pathToTemplates = "./templates"
+
+	for _, p := range pageTests {
+		rr := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", p.url, nil)
+
+		ctx := getCtx(req)
+		req = req.WithContext(ctx)
+
+		if len(p.sessionData) > 0 {
+			for key, value := range p.sessionData {
+				testApp.Sessions.Put(ctx, key, value)
+			}
+		}
+
+		p.handler.ServeHTTP(rr, req)
+
+		if rr.Code != p.expectedStatusCode {
+			t.Errorf("%s failed: expected %d, but got %d", p.name, p.expectedStatusCode, rr.Code)
+		}
+
+		if len(p.expectedHTML) > 0 {
+			html := rr.Body.String()
+			if !strings.Contains(html, p.expectedHTML) {
+				t.Errorf("%s, failed: expected to find %s, but did not ", p.name, p.expectedHTML)
+			}
+		}
+	}
+}
